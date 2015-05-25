e03e36509c70155f7f07681f46491235a6149bf2
Teng Li <otec.teng@gmail.com>
2015-05-25 10:10:32 +0800

    change view

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 var config = require('../config');
 var _ = require('lodash');
+  var isAuthenticated = function (req, res, next) {
+    if (req.isAuthenticated())
+      return next();
+    res.redirect('/login');
+  };
 
 module.exports = function(passport){
+  router.get('/', isAuthenticated, function(req, res){
+    res.render('desktop.ejs', { user: req.user });
+  });
+  
   router.get('/config', function (req, res, next) {
     var keys = [
       'kibana_index',
@@ -15,13 +24,16 @@ module.exports = function(passport){
     res.json(data);
   });
 
+
   router.get('/login', function(req, res) {
     res.render('login');
   });
-
+  router.get('/desktop', function(req, res) {
+    res.render('desktop.ejs');
+  });
   router.post('/login',
     passport.authenticate('local',{
-    successRedirect: '/desktop.html',
+    successRedirect: '/desktop',
     failureRedirect: '/login'}
   ));
   return router;
